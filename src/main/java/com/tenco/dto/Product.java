49d25743960ac7c85ab730587af23b27c13f256e
wing853 <qwqhsk@naver.com>
2026-04-09 10:47:31 +0900

    feat: admin, product, sales dto 구현

--- src/main/java/com/tenco/dto/Product.java
+++ src/main/java/com/tenco/dto/Product.java
@@ -1,4 +1,17 @@
 package com.tenco.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Product {
+    private int id;
+    private String barcode;
+    private String name;
+    private String category;
+    private BigDecimal price;
+    private BigDecimal cost;
+    private int stock;
+    private int minStock;
+    private LocalDate expireDate;
+    private boolean isActive;
 }
